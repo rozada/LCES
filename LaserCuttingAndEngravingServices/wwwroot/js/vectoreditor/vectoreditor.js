32cfc7c28f5cbe8e91a2b79b6e581f53bcd4c648
Erwin Rozada <erozada@yahoo.com>
2019-12-22 17:00:57 -0500

    in progress

--- LaserCuttingAndEngravingServices/wwwroot/js/vectoreditor/vectoreditor.js
+++ LaserCuttingAndEngravingServices/wwwroot/js/vectoreditor/vectoreditor.js
@@ -70,8 +70,33 @@
 
     //#region textbox methods
 
-    VectorEditor.prototype.createTextBox = function (id) {
+    VectorEditor.prototype.createTextBox = function (id, textEdit, svgDiv) {
         var self = this;
+        var pos = self.getPreviousTextEditPos(id); 
+        pos.left += 35;
+        pos.top += 35;
+        console.log(pos);
+
+        $(textEdit).parent().css({ position: 'relative' });
+        $(textEdit).css({ top: pos.top, left: pos.left, position: 'absolute' });
+        $(textEdit).attr("id", "tbdiv" + id);
+        //textEdit.style.cssText = "border-style: solid; height: 75px; width: 300px;background-color: orange";
+        $(svgDiv).append($(textEdit));
+        var tbdivheader = $(textEdit).find("#tbdivheader");
+        var editTextContainer = $(textEdit).find("#editTextContainer");
+        console.log("tbdivheader ");
+        console.log(tbdivheader);
+        $(tbdivheader).attr("id", "tbdiv" + id + "header");
+        $(editTextContainer).attr("id", "editTextContainer" + id);
+        console.log(tbdivheader);
+
+        textEdit[0].dragElement = new dragElement(textEdit[0]);
+        //textEdit[0].dragElement(textEdit[0]);
+
+        //svgDiv.appendChild(textEdit);
+
+
+        return;
         var text = this.svgOwnerDocument.createElementNS(svgns, "text");
         text.setAttributeNS(null, "x", 30);
         text.setAttributeNS(null, "y", 90);
@@ -86,11 +111,30 @@
         
         text.textContent = "(new Text Box)";
         text.style.zIndex = 15;
-        self.svg.appendChild(text);
+        //textEdit.appendChild(text);
+        self.svg.appendChild(text);//Edit);
+       
+        //self.svg.appendChild(text);
 
         console.log(text);
     }
 
+    VectorEditor.prototype.getPreviousTextEditPos = function (id) {
+        var left = parseInt($("#" + "tbdiv" + (id - 1)).css("left"));
+        if (!left) {
+            left = 0;
+        }
+        console.log("left");
+        console.log(left);
+
+        var top = parseInt($("#" + "tbdiv" + (id - 1)).css("top"));
+        if (!top) {
+            top = 0;
+        }
+
+        var pos = { left: left, top: top };
+        return pos;
+    }
 
     VectorEditor.prototype.setTextBoxContent = function (textContent, id) {
         var self = this;
@@ -264,4 +308,51 @@
         global.vectorEditor.svgOwnerDocument = svgs[0].ownerDocument;
     }
     console.log(global.vectorEditor);
-})(window);
+
+    function dragElement(elmnt) {
+        console.log("Elmnt ");
+        console.log(elmnt);
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        console.log("Pos1: " + pos1 + ", Pos2 :" + pos2 + ", Pos3: " + pos3 + ", Pos4: " + pos4);
+        if (document.getElementById(elmnt.id + "header")) {
+            console.log("present: " + elmnt.id + "header");
+            /* if present, the header is where you move the DIV from:*/
+            document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+        } else {
+            /* otherwise, move the DIV from anywhere inside the DIV:*/
+            elmnt.onmousedown = dragMouseDown;
+        }
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+            /* stop moving when mouse button is released:*/
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
+
+}
+)(window);
